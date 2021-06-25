e04d509601549a439c2460bada3127e6c5428d8c
Tusharshrivastav <ts3657@gmail.com>
2021-06-25 20:22:22 +0530

    setup the starter code

---
+++ index.js
@@ -0,0 +1,23 @@
+//Library include
+require("dotenv").config();
+
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const morgan = require("morgan");
+const PORT = process.env.PORT || 5000;
+
+// Middlewares
+app.use(cors());
+app.use(express.json());
+app.use(morgan("dev"));
+
+//Route
+app.get("/", (req, res) => {
+  return res.status(200).json({ status: true,"message":"Amazon clone home page" });
+});
+
+//Server Listen
+app.listen(PORT, () => {
+  console.log(`Server is running on ${PORT} `);
+});
