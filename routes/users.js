9661dd5916974f3a9e495d8991406e84d3a545bc
Tusharshrivastav <ts3657@gmail.com>
2021-06-26 07:58:31 +0530

    register api setup

--- routes/users.js
+++ routes/users.js
@@ -8,11 +8,31 @@ const moment = require("moment");
 const User = require("../config/database");
 const Token_Key = process.env.Token;
 
-//default route
-router.get("/login", (req, res) => {
-  return res.status(200).json({
-    status: true,
-    Message: "Kaam kar rha hai bawa",
-  });
-});
+// user reister route
+// Access public
+// url :http://localhost:5000/api/users/register
+router.post(
+  "/register",
+  [
+    // check empty fields
+    check("username").not().isEmpty().trim().escape(),
+    check("password").not().isEmpty().trim().escape(),
+    //check email
+    check("email").isEmail().normalizeEmail().withMessage(""),
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    // check error is not empty
+    if (!errors.isEmpty()) {
+      return res.status(400).json({
+        status: false,
+        errors: errors.array(),
+      });
+    }
+    return res.status(200).json({
+      status: true,
+      data: req.body,
+    });
+  }
+);
 module.exports = router;
